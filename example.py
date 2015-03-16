4e72216afe863cf710e780d7467a4ee8497377be
Christopher Night <cosmologicon@gmail.com>
2015-03-15 23:50:45 -0400

    Clooky Clunker example game Fonts from Google Web Fonts added: Boogaloo Bubblegum Sans Cherry Cream Soda Roboto Condensed

--- example.py
+++ example.py
@@ -12,20 +12,17 @@ sx, sy = 854, 480
 screen = pygame.display.set_mode((sx, sy))
 pygame.display.set_caption("Clooky Clunker")
 
-playing = True
-score = 0
-totalscore = 0
-fscore = 0
-clunkers = 0
+score, totalscore, clunkers = 0, 0, 0
 nextgoal = 0
 tgoal = -100
 infos = []
 t = 0
 
-buttonrects = [pygame.Rect((20, 120 + 85 * j, 220, 70)) for j in range(4)]
+buttonrects = [pygame.Rect((50, 120 + 85 * j, 180, 70)) for j in range(4)]
 buttonnames = ["auto-clunker", "clunkutron", "turbo enclunkulator", "clunx capacitor"]
-buttoncosts = [10, 400, 20000, 1000000]
+buttoncosts = [10, 400, 12000, 250000]
 
+playing = True
 while playing:
 	nextt = 0.001 * pygame.time.get_ticks()
 	t, dt = nextt, nextt - t
@@ -54,10 +51,8 @@ while playing:
 					clunkers += 10 ** j
 					buttoncosts[j] += int(round(cost * 0.2))
 
-	fscore += clunkers * dt
-	score += int(fscore)
-	totalscore += int(fscore)
-	fscore -= int(fscore)
+	score += clunkers * dt
+	totalscore += clunkers * dt
 
 	if totalscore > 100 * (1 << nextgoal):
 		goaltext = "Achievement unlocked:\nCL%sKY!" % ("O" * (nextgoal + 2))
@@ -73,24 +68,24 @@ while playing:
 		text = u"%s: %d\u00A0clunks" % (name, cost)
 		color = "white" if cost <= score else "#666666"
 		box = rect.inflate(-16, -16)
-		ptext.drawbox(text, box, fontname=None, lineheight=0.9, color=color, owidth=0.5)
+		ptext.drawbox(text, box, fontname="Bubblegum_Sans", lineheight=0.9, color=color, owidth=0.5)
 	hudtext = "\n".join([
 		"time played: %d" % t,
 		"clunks: %d" % score,
 		"all-time clunks: %d" % totalscore,
 		"clunks per second: %d" % clunkers,
 	])
-	ptext.draw(hudtext, right=sx-10, top=120, fontname="Lobster-Regular", fontsize=32,
-		color="black", ocolor="#777777", owidth=0.5, lineheight=1.3)
+	ptext.draw(hudtext, right=sx-10, top=120, fontname="Roboto_Condensed", fontsize=32,
+		color="#00AA00", scolor="#005500", shadow=(-1,1), lineheight=1.3)
 	ptext.draw("Clooky Clunker", midtop=(sx/2, 10), fontname="CherryCreamSoda", fontsize=64,
 		owidth=1.2, color="#884400", gcolor="#442200")
 	for it, ix, iy, text in infos:
 		dt = t - it
-		ptext.draw(text, fontname=None, fontsize=22, center=(ix, iy-60*dt), alpha=1-dt, shadow=(1,1))
+		ptext.draw(text, fontname=None, fontsize=28, center=(ix, iy-60*dt), alpha=1-dt, shadow=(1,1))
 	infos = filter((lambda info: t - info[0] < 1), infos)
 	if t - tgoal < 2:
 		alpha = min(2 - (t - tgoal), 1)
-		ptext.draw(goaltext, fontname=None, fontsize=48, bottom=sy-20, right=sx-40,
+		ptext.draw(goaltext, fontname="Boogaloo", fontsize=48, bottom=sy-20, right=sx-40,
 			color="#AAAAFF", gcolor="#4444AA", shadow=(1.5,1.5), alpha=alpha, textalign="center")
 	pygame.display.flip()
 
